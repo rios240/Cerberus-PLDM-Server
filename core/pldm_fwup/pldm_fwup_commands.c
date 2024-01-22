05b69b5a8d06881d485a8d569208b460e0b0aaf7
Tyler <tylerios@colostate.edu>
2024-01-22 14:23:09 -0700

    RequestUpdate, PassComponentTable

--- core/pldm_fwup/pldm_fwup_commands.c
+++ core/pldm_fwup/pldm_fwup_commands.c
@@ -7,63 +7,6 @@
 #include "firmware_update.h"
 
 
-/*
-struct firmware_update_package pldm_fup;
-
-int initialize_firmware_update_package() {
-
-    const char *comp_image_set_version_str = "cerberus_v2.0";
-    uint8_t fw_device_pkg_data[10];
-    for (int i = 0; i < 10; i++) {
-        fw_device_pkg_data[i] = rand() % 256;
-    }
-
-    struct descriptor_tlv descriptors[2];
-
-    int iana_number = 135;
-    descriptors[0].descriptor_type = 0;
-    descriptors[0].descriptor_length = 2;
-    memcpy(descriptors[0].descriptor_data, &iana_number, descriptors[0].descriptor_length);
-
-
-    const char *uuid_string = "8d759454a78111eea5060242ac120002";
-    descriptors[1].descriptor_type = 2;
-    descriptors[1].descriptor_length = 16;
-    memcpy(descriptors[1].descriptor_data, &uuid_string, descriptors[1].descriptor_length);
-
-
-
-    struct firmware_device_id_record record;
-    record.descriptor_count = 2;
-    record.device_update_option_flags.value = 1;
-    record.comp_image_set_version_string_type = 1;
-    record.comp_image_set_version_string_length = strlen(comp_image_set_version_str);
-    record.fw_device_pkg_data_length = 10;
-    record.applicable_components.byte = 0x00;
-    record.applicable_components.bits.bit0 = 1;
-    record.applicable_components.bits.bit1 = 1;
-    record.comp_image_set_version_str = (const uint8_t *)comp_image_set_version_str;
-    record.record_descriptors = descriptors;
-    record.device_package_data = fw_device_pkg_data;
-
-    const char *comp_version_string = "bios_v2.0";
-
-    struct component_image_information comp_info[1];
-    comp_info[0].comp_classification = COMP_FIRMWARE_OR_BIOS;
-    comp_info[0].comp_identifier = rand() % 65536;
-    comp_info[0].comp_options.value = 0;
-    comp_info[0].comp_options.bits.bit0 = 1;
-    comp_info[0].comp_comparison_stamp = 0xFFFFFFF;
-    comp_info[0].requested_comp_activation_method.value = 0;
-    comp_info[0].requested_comp_activation_method.bits.bit0 = 0x1b;
-    
-
-    return 0;
-
-}
-
-*/
-
 int issue_query_device_identifiers(uint8_t *request, size_t *payload_length)
 {
 
@@ -123,7 +66,8 @@ int process_get_firmware_parameters(struct cmd_interface *intf, struct cmd_inter
     struct variable_field active_comp_image_set_ver_str;
     struct variable_field pending_comp_image_set_ver_str;
 
-    int status = decode_get_firmware_parameters_comp_img_set_resp(respMsg, response->length, &resp_data,
+    const size_t payload_length = response->length - sizeof (struct pldm_msg_hdr) - 1;
+    int status = decode_get_firmware_parameters_comp_img_set_resp(respMsg, payload_length, &resp_data,
                                                         &active_comp_image_set_ver_str, &pending_comp_image_set_ver_str);
     
     struct component_parameter_table comp_parameter_table;
@@ -132,10 +76,10 @@ int process_get_firmware_parameters(struct cmd_interface *intf, struct cmd_inter
 
     const uint8_t *comp_parameter_table_data = respMsg->payload + sizeof (struct get_firmware_parameters_resp) + active_comp_image_set_ver_str.length
                                             + pending_comp_image_set_ver_str.length;
-    const size_t payload_length = response->length - sizeof (struct get_firmware_parameters_resp) - active_comp_image_set_ver_str.length
+    const size_t comp_parameter_table_length = response->length - sizeof (struct get_firmware_parameters_resp) - active_comp_image_set_ver_str.length
                                             - pending_comp_image_set_ver_str.length - 1;
 
-    status = decode_get_firmware_parameters_comp_resp(comp_parameter_table_data, payload_length, &comp_parameter_table, 
+    status = decode_get_firmware_parameters_comp_resp(comp_parameter_table_data, comp_parameter_table_length, &comp_parameter_table, 
                                                 &active_comp_ver_str, &pending_comp_ver_str);
     
     response->length = 0;
@@ -143,4 +87,105 @@ int process_get_firmware_parameters(struct cmd_interface *intf, struct cmd_inter
     return status;
 
 
+}
+
+
+int issue_request_update(uint8_t *request, size_t *payload_length)
+{
+    uint8_t instance_id = 1;
+    const char* comp_image_set_ver_str_arr = "cerberus_v2.0";
+
+    struct request_update_req req_data;
+    req_data.max_transfer_size = 60;
+    req_data.no_of_comp = 1;
+    req_data.max_outstand_transfer_req = 1;
+    req_data.comp_image_set_ver_str_type = PLDM_COMP_ASCII;
+    req_data.comp_image_set_ver_str_len = strlen(comp_image_set_ver_str_arr);
+
+    struct variable_field comp_image_set_ver_str;
+    comp_image_set_ver_str.ptr = (const uint8_t *)comp_image_set_ver_str_arr;
+    comp_image_set_ver_str.length = req_data.comp_image_set_ver_str_len;
+
+    *payload_length = sizeof (struct pldm_msg_hdr)
+                    + sizeof (struct request_update_req)
+                    + comp_image_set_ver_str.length
+                    + 1;
+
+    const size_t pldm_payload_length = sizeof (struct request_update_req) + comp_image_set_ver_str.length;
+
+    request[0] = MCTP_BASE_PROTOCOL_MSG_TYPE_PLDM;
+    struct pldm_msg *reqMsg = (struct pldm_msg *)(request + 1);
+
+    int status = encode_request_update_req(instance_id, reqMsg, pldm_payload_length, &req_data, &comp_image_set_ver_str);
+
+    return status;
+}
+
+
+int process_request_update(struct cmd_interface *intf, struct cmd_interface_msg *response)
+{
+    struct pldm_msg *respMsg = (struct pldm_msg *)(&response->data[1]);
+
+    uint8_t completion_code;
+    uint16_t fd_meta_data_len;
+    uint8_t fd_pkg_data;
+
+    const size_t payload_length = response->length - sizeof (struct pldm_msg_hdr) - 1;
+    int status = decode_request_update_resp(respMsg, payload_length, &completion_code, &fd_meta_data_len, &fd_pkg_data);
+
+    response->length = 0;
+
+    return status;
+
+}
+
+int issue_pass_component_table(uint8_t *request, size_t *payload_length)
+{
+    uint8_t instance_id = 1;
+
+    const char *comp_ver_str_arr = "BIOS_v2.0";
+
+    struct pass_component_table_req req_data;
+    req_data.transfer_flag = 0x5;
+    req_data.comp_classification = COMP_FIRMWARE_OR_BIOS;
+    req_data.comp_identifier = 0xDEAD;
+    req_data.comp_classification_index = 0xDE;
+    req_data.comp_comparison_stamp = 0xDEADBEEF;
+    req_data.comp_ver_str_type = PLDM_COMP_ASCII;
+    req_data.comp_ver_str_len = strlen(comp_ver_str_arr);
+
+    struct variable_field comp_ver_str;
+    comp_ver_str.ptr = (const uint8_t *)comp_ver_str_arr;
+    comp_ver_str.length = req_data.comp_ver_str_len;
+
+    *payload_length = sizeof (struct pldm_msg_hdr)
+                    + sizeof (struct pass_component_table_req)
+                    + comp_ver_str.length
+                    + 1;
+
+    const size_t pldm_payload_length = sizeof (struct pass_component_table_req) + comp_ver_str.length;
+
+    request[0] = MCTP_BASE_PROTOCOL_MSG_TYPE_PLDM;
+    struct pldm_msg *reqMsg = (struct pldm_msg *)(request + 1);
+
+    int status = encode_pass_component_table_req(instance_id, reqMsg, pldm_payload_length, &req_data, &comp_ver_str);
+
+    return status;
+}
+
+
+int process_pass_component_table(struct cmd_interface *intf, struct cmd_interface_msg *response)
+{
+    struct pldm_msg *respMsg = (struct pldm_msg *)(&response->data[1]);
+
+    uint8_t completion_code;
+    uint8_t comp_resp;
+    uint8_t comp_resp_code;
+
+    const size_t payload_length = response->length - sizeof (struct pldm_msg_hdr) - 1;
+    int status = decode_pass_component_table_resp(respMsg, payload_length, &completion_code, &comp_resp, &comp_resp_code);
+
+    response->length = 0;
+
+    return status;
 }
--- core/pldm_fwup/pldm_fwup_interface.c
+++ core/pldm_fwup/pldm_fwup_interface.c
@@ -10,6 +10,11 @@
 #include "mctp/mctp_interface.h"
 #include "pldm_fwup_interface.h"
 
+uint8_t *realloc_buf(uint8_t *ptr, size_t length) {
+    uint8_t *temp = realloc(ptr, length * sizeof (uint8_t));
+    return temp;
+}
+
 int initialize_firmware_update(struct mctp_interface *mctp, struct cmd_channel *cmd_channel, 
                         struct cmd_interface *cmd_mctp, 
                         struct cmd_interface *cmd_spdm, struct cmd_interface *cmd_cerberus,
@@ -39,27 +44,90 @@ int initialize_firmware_update(struct mctp_interface *mctp, struct cmd_channel *
 
 int perform_firmware_update(struct mctp_interface *mctp, struct cmd_channel *cmd_channel)
 {
-    uint8_t *pldm_buf = (uint8_t *)malloc(PLDM_MAX_PAYLOAD_LENGTH + sizeof (uint8_t));
+    uint8_t *pldm_payload = (uint8_t *)malloc(PLDM_MAX_PAYLOAD_LENGTH * sizeof (uint8_t));
     size_t payload_length = 0;
 
-    int status = issue_query_device_identifiers(pldm_buf, &payload_length);
+    int status = issue_query_device_identifiers(pldm_payload, &payload_length);
     if (status != 0) {
+        free(pldm_payload);
         return status;
     }
-    uint8_t pldm_request[payload_length];
-    memcpy(pldm_request, pldm_buf, payload_length);
-    free(pldm_buf);
-
+    pldm_payload = realloc_buf(pldm_payload, payload_length);
     uint8_t mctp_buf[MCTP_BASE_PROTOCOL_MAX_MESSAGE_LEN];
-
-    status = mctp_interface_issue_request(mctp, cmd_channel, DEST_ADDR, DEST_EID, pldm_request, payload_length,
+    status = mctp_interface_issue_request(mctp, cmd_channel, DEST_ADDR, DEST_EID, pldm_payload, payload_length,
                                     mctp_buf,  MCTP_BASE_PROTOCOL_MAX_MESSAGE_LEN, 0);
     if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    mctp->cmd_mctp->process_response = process_query_device_identifiers;
+    status = cmd_channel_receive_and_process(cmd_channel, mctp, MS_TIMEOUT);
+    if (status != 0) {
+        free(pldm_payload);
         return status;
     }
 
-    mctp->cmd_mctp->process_response = process_query_device_identifiers;
 
+    pldm_payload = realloc_buf(pldm_payload, PLDM_MAX_PAYLOAD_LENGTH);
+    status = issue_get_firmware_parameters(pldm_payload, &payload_length);
+    if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    pldm_payload = realloc_buf(pldm_payload, payload_length);
+    status = mctp_interface_issue_request(mctp, cmd_channel, DEST_ADDR, DEST_EID, pldm_payload, payload_length,
+                                    mctp_buf,  MCTP_BASE_PROTOCOL_MAX_MESSAGE_LEN, 0);
+    if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    mctp->cmd_mctp->process_response = process_get_firmware_parameters;
     status = cmd_channel_receive_and_process(cmd_channel, mctp, MS_TIMEOUT);
+    if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+
+
+    pldm_payload = realloc_buf(pldm_payload, PLDM_MAX_PAYLOAD_LENGTH);
+    status = issue_request_update(pldm_payload, &payload_length);
+    if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    pldm_payload = realloc_buf(pldm_payload, payload_length);
+    status = mctp_interface_issue_request(mctp, cmd_channel, DEST_ADDR, DEST_EID, pldm_payload, payload_length,
+                                    mctp_buf,  MCTP_BASE_PROTOCOL_MAX_MESSAGE_LEN, 0);
+     if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    mctp->cmd_mctp->process_response = process_request_update;
+    status = cmd_channel_receive_and_process(cmd_channel, mctp, MS_TIMEOUT);
+    if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+
+
+    pldm_payload = realloc_buf(pldm_payload, PLDM_MAX_PAYLOAD_LENGTH);
+    status = issue_pass_component_table(pldm_payload, &payload_length);
+    if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    pldm_payload = realloc_buf(pldm_payload, payload_length);
+    status = mctp_interface_issue_request(mctp, cmd_channel, DEST_ADDR, DEST_EID, pldm_payload, payload_length,
+                                    mctp_buf,  MCTP_BASE_PROTOCOL_MAX_MESSAGE_LEN, 0);
+     if (status != 0) {
+        free(pldm_payload);
+        return status;
+    }
+    mctp->cmd_mctp->process_response = process_pass_component_table;
+    status = cmd_channel_receive_and_process(cmd_channel, mctp, MS_TIMEOUT);
+
+
+    free(pldm_payload);
     return status;
+
 }
